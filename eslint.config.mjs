de7e9c355e6858c1c7d628bb808f5c69f421e837
sunil <sunil.shastry69@gmail.com>
2025-09-21 17:45:55 -0600

    Add custom eslint rules and recommendations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
+import { defineConfig, globalIgnores } from 'eslint/config';
+import pluginVue from 'eslint-plugin-vue';
+import { vueTsConfigs } from '@vue/eslint-config-typescript';
+
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt();
+export default withNuxt(
+	defineConfig(
+		{
+			name: 'app/files-to-lint',
+			files: ['**/*.{vue,ts,tsx,mts}'],
+		},
+		globalIgnores(['**/dist/**']),
+		pluginVue.configs['flat/strongly-recommended'],
+		vueTsConfigs.recommended
+	)
+);
 // Your custom configs here
