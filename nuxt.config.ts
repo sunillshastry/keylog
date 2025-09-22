d6724d0c9c1af14ad67d09c9ad74c81744bbdbc8
sunil <sunil.shastry69@gmail.com>
2025-09-21 18:28:50 -0600

    Redefine options in nuxt.config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2025-07-15',
+	modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxt/scripts'],
 	devtools: { enabled: true },
 
-	modules: [
-		'@nuxt/eslint',
-		'@nuxt/fonts',
-		'@nuxt/icon',
-		'@nuxt/image',
-		'@nuxt/scripts',
-	],
+	eslint: {
+		config: {
+			stylistic: false,
+		},
+	},
 });
